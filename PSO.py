4c65e6e64bf352e1c8e2ac26d1dac4c1d311164e
DarongDG <84388466+DarongDG@users.noreply.github.com>
2022-02-05 21:13:51 +0100

    added experiments

--- PSO.py
+++ PSO.py
@@ -1,37 +1,19 @@
-import numpy as np
 
-from InertiaStrategies import ChaoticDescendingInertia, RandomInertiaEvolutionaryStrategy, LinearInertia, \
-    DynamicAdaptiveStrategy
 from Particle import Particle, Globals
 import json
-
-
-def f(x):
-    # simple function
-    return 2 * x[0] ** 2 - 8 * x[0] + 1
-
-
-def f_rosenbrock(x):
-    a = 0
-    b = 1
-    return (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
-
-
-def f_rastrigin(x):
-    value = (x[0] ** 2 - 10 * np.cos(2 * np.pi * x[0])) + \
-            (x[1] ** 2 - 10 * np.cos(2 * np.pi * x[1])) + 20
-    return value
 
 
 def PSO(objective_function, n_particles, iterations, n_param, inertia_strategy):
     # initialize the particles
     particles = []
+
     globals = Globals(n_param)
     for i in range(n_particles):
         particle = Particle(globals, objective_function)
         particles.append(particle)
 
     positions = []
+    global_best_history = []
 
     for i in range(iterations):
         positions.append([])
@@ -39,8 +21,8 @@ def PSO(objective_function, n_particles, iterations, n_param, inertia_strategy):
             w = inertia_strategy.get_inertia(i, particles)
             particle.update(w)
             positions[i].append({'position': particle.position, 'velocity': particle.velocity})
+        global_best_history.append(globals.best_value)
 
-        # print("iteration {} best at {}".format(particles[0].get_g_best_value(), particles[0].get_g_best_position()))
     with open('plot/position.json', 'w') as f:
         json.dump(positions, f, indent=4)
     print(
@@ -48,18 +30,7 @@ def PSO(objective_function, n_particles, iterations, n_param, inertia_strategy):
             globals.best_value, globals.best_position
         )
     )
-    return globals.best_value, globals.best_position
+    return global_best_history, globals.best_value, globals.best_position
 
 
-# Running the PSO
 
-value, position = PSO(
-    objective_function=f_rosenbrock,
-    n_param=2,
-    n_particles=10,
-    iterations=100,
-    inertia_strategy=RandomInertiaEvolutionaryStrategy()
-)
-# gives the wrong position but correct minimum
-print(position)
-print(f_rosenbrock(position))

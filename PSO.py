a84d194c602e4989177d847f16793fa65789f7f5
giulioZani <giulio.zani@gmail.com>
2022-02-07 01:20:48 +0100

    update

--- PSO.py
+++ PSO.py
@@ -1,6 +1,9 @@
 import random
 
-from InertiaStrategies import DynamicAdaptiveStrategy, RandomInertiaEvolutionaryStrategy
+from InertiaStrategies import (
+    DynamicAdaptiveStrategy,
+    RandomInertiaEvolutionaryStrategy,
+)
 from ObjectiveFunctions import f_rosenbrock
 from Particle import Particle, Globals
 import json
@@ -11,16 +14,25 @@ def f(x):
     return 2 * x[0] ** 2 - 8 * x[0] + 1
 
 
-def PSO(objective_function, n_particles, iterations, n_param,
-        inertia_strategy, guess_random_range, gradient_coef=0,
-        seed=random.randint(0, 412312312312)):
+def PSO(
+    objective_function,
+    n_particles,
+    iterations,
+    n_param,
+    inertia_strategy,
+    guess_random_range,
+    gradient_coef=0,
+    seed=random.randint(0, 412312312312),
+):
     # initialize the particles
     particles = []
     random.seed(seed)
 
     globals = Globals(n_param)
     for i in range(n_particles):
-        particle = Particle(globals, objective_function, guess_random_range, gradient_coef)
+        particle = Particle(
+            globals, objective_function, guess_random_range, gradient_coef
+        )
         particles.append(particle)
 
     positions = []
@@ -31,10 +43,12 @@ def PSO(objective_function, n_particles, iterations, n_param,
         for particle in particles:
             w = inertia_strategy.get_inertia(i, particles)
             particle.update(w)
-            positions[i].append({'position': particle.position, 'velocity': particle.velocity})
+            positions[i].append(
+                {"position": particle.position, "velocity": particle.velocity}
+            )
         global_best_history.append(globals.best_value)
 
-    with open('plot/position.json', 'w') as f:
+    with open(f"plot/{objective_function.__name__}.json", "w") as f:
         json.dump(positions, f, indent=4)
     print(
         "found minimum : {} at {}".format(
@@ -44,14 +58,20 @@ def PSO(objective_function, n_particles, iterations, n_param,
     return global_best_history, globals.best_value, globals.best_position
 
 
-(history, best, position) = PSO(objective_function=f_rosenbrock,
-                                n_param=2,
-                                n_particles=10,
-                                iterations=100,
-                                inertia_strategy=RandomInertiaEvolutionaryStrategy(),
-                                guess_random_range=70,
-                                gradient_coef=0.5
-                                )
+def main():
+    (history, best, position) = PSO(
+        objective_function=f_rosenbrock,
+        n_param=2,
+        n_particles=10,
+        iterations=100,
+        inertia_strategy=RandomInertiaEvolutionaryStrategy(),
+        guess_random_range=70,
+        gradient_coef=0.5,
+    )
 
-print(position)
-print(best)
+    print(position)
+    print(best)
+
+
+if __name__ == "__main__":
+    main()
--- plot/main.js
+++ plot/main.js
@@ -1,61 +1,140 @@
 const drawPlot = async () => {
-  const rawData = await (await fetch("positions.json")).json();
+	const objectiveFuntionName = 'rastrigin'
+  const rawData = await (await fetch(`f_${objectiveFuntionName}.json`)).json();
   const minPositionX = Math.min(
     ...rawData.flatMap((x) => x.map((y) => y.position[0]))
   );
   const minPositionY = Math.min(
-    ...rawData.flatMap((x) => x.map((y) => y.position[0]))
+    ...rawData.flatMap((x) => x.map((y) => y.position[1]))
   );
   const maxPositionX = Math.max(
     ...rawData.flatMap((x) => x.map((y) => y.position[0]))
   );
   const maxPositionY = Math.max(
-    ...rawData.flatMap((x) => x.map((y) => y.position[0]))
+    ...rawData.flatMap((x) => x.map((y) => y.position[1]))
   );
-
-  /*
-  def f_rosenbrock(x):
- 13     a = 0
- 14     b = 1
- 15     return (a - x[0]) ** 2 + b * (x[1] - x[0] ** 2) ** 2
- */
+	const rastrigin = (x)=>x.reduce((acc, val)=>acc + Math.pow(val, 2) - 10 * Math.cos(2*Math.PI*val), 0) + 20
   const rosenbrock = (x) => {
     const a = 0;
     const b = 1;
     return Math.pow(a - x[0], 2) + b * Math.pow(x[1] - Math.pow(x[0], 2), 2);
   };
-
-  var z_data = [];
-  let x = minPositionX;
-  const pixDensity = 100;
+	const objectiveFuntions = {
+		rastrigin,
+		rosenbrock
+	}
+	const chosenFuntion = objectiveFuntions[objectiveFuntionName];
+  const z_data = [];
+  const pixDensity = 150;
   const xStep = (maxPositionX - minPositionX) / pixDensity;
   const yStep = (maxPositionY - minPositionY) / pixDensity;
   for (let i = 0; i < pixDensity; i++) {
+		const row = []
     for (let j = 0; j < pixDensity; j++) {
-      z_data.push(rosenbrock([xStep * i, yStep * j]));
+			const x = minPositionX + xStep * i
+			const y = minPositionY + yStep * j
+      row.push(chosenFuntion([x, y]));
     }
+		z_data.push(row)
   }
-
-  const data = [
-    {
-      z: z_data,
+	const min = Math.min(...z_data.flatMap(r=>r))
+	const max = Math.max(...z_data.flatMap(r=>r))
+	const normalizeZ = y => (y-min)/(max - min)
+	const fixedZData = z_data.map(x=>x.map(normalizeZ))
+	const trace1 = {
+      z: fixedZData,
       type: "surface",
-    },
-  ];
+    }
+  
+	/*
+	 {
+      x: 2,
+      y: 5,
+      xref: 'x',
+      yref: 'y',
+      text: 'Annotation Text',
+      showarrow: true,
+      arrowhead: 7,
+      ax: 0k,
+      ay: -40
+    }
 
+		var trace1 = {
+	x:unpack(rows, 'x1'), y: unpack(rows, 'y1'), z: unpack(rows, 'z1'),
+	mode: 'markers',
+	marker: {
+		size: 12,
+		line: {
+		color: 'rgba(217, 217, 217, 0.14)',
+		width: 0.5},
+		opacity: 0.8},
+	type: 'scatter3d'
+};
+		*/
+
+	const ballShift = (max - min)/45
+	const normalizeX = x => (x-minPositionX)/(maxPositionX-minPositionX)
+	const normalizeY = x => (x-minPositionY)/(maxPositionY-minPositionY)
+	const trace2 = {
+		x:rawData[0].map(p=>pixDensity*normalizeY(p.position[1])),
+		y:rawData[0].map(p=>pixDensity*normalizeX(p.position[0])),
+		z:rawData[0].map(p=>normalizeZ(chosenFuntion(p.position)) + ballShift),
+		mode:'markers',
+				marker: {
+						size: 12,
+						line: {
+							color: 'rgba(217, 217, 217, 0.14)',
+							width: 0.5
+						},
+						opacity: 0.8
+				},
+				type: 'scatter3d'
+	}
+	const data = [
+    trace1,
+		trace2
+	];
   var layout = {
-    title: "Mt Bruno Elevation",
-    autosize: false,
-    width: 1000,
-    height: 1000,
-    margin: {
+    title: "Mt ",
+    autosize: true,
+    width: window.innerWidth,
+    height: window.innerHeight,
+		scene:{
+			camera:{
+				eye: objectiveFuntionName === 'rastrigin' ? {x:0.8, y:-0.8, z:1.5} : {}
+			}
+		},
+		margin: {
       l: 65,
       r: 50,
       b: 65,
       t: 90,
     },
   };
-  Plotly.newPlot("plot", data, layout);
-};
+	const plot = document.getElementById('plot')
+  await Plotly.newPlot(plot, data, layout);
+	let i = 1
+	const updatePlot = async ()=>{
+		if (i < rawData.length){
+				//trace2['x'] = rawData[i].map(p=>pixDensity*normalizeY(p.position[1])),
+			  //trace2['y'] = rawData[i].map(p=>pixDensity*normalizeX(p.position[0])),
+				//trace2['z'] = rawData[i].map(p=>normalizeZ(chosenFuntion(p.position))),
+				//await Plotly.redraw(plot)
 
+			await Plotly.animate('plot', {
+					data: [{
+						x:rawData[i].map(p=>pixDensity*normalizeY(p.position[1])),
+						y:rawData[i].map(p=>pixDensity*normalizeX(p.position[0])),
+						z:rawData[i].map(p=>normalizeZ(chosenFuntion(p.position) + ballShift)),
+					}],
+					traces: [1],
+				})
+			i += 1
+		}
+		else{
+			window.clearInterval(interval)
+		}
+	}
+	const interval = window.setInterval(updatePlot, 500)
+}
 await drawPlot();
